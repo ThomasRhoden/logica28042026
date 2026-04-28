0b9d1108d5387f5db626df50cdf20bf1b5841dde
ThomasRhoden <rhodenthomas@gmail.com>
2026-04-28 20:21:17 -0300

    função 5

---
+++ main5.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+void verificarAprovacao(float nota){
+    if(nota >= 7){
+        std::cout<<"Status: Aprovado"<<std::endl;
+    }else{
+        std::cout<<"Status: Reprovado!"<<std::endl;
+    }
+}
+
+int main(){
+
+    float minhaNota = 8.5;
+
+    verificarAprovacao(minhaNota);
+
+    return 0;
+}
