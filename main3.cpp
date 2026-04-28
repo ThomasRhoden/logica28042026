bed447a321c3a996e6420a088caec4c669b46dbc
ThomasRhoden <rhodenthomas@gmail.com>
2026-04-28 20:00:23 -0300

    commit 3

---
+++ main3.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+int main(){
+
+    int myNumbers[5] = {1, 2, 3, 4, 5};
+
+    for(int i = 0; i < 5; i++){
+        std::cout<<myNumbers[i]<<std::endl;
+    }
+
+    return 0;
+}
