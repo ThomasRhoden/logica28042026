f3a8fe9a563a4387c80eb72e173d7bdefd9b6cfe
ThomasRhoden <rhodenthomas@gmail.com>
2026-04-28 19:37:01 -0300

    commit vector

---
+++ main2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+
+int main(){
+
+    std::vector<std::string>cars = {"Volvo", "BMW", "Mercedes"};
+
+    cars.push_back("Mazda");
+
+    for(std::string car : cars){
+        std::cout<<car<<std::endl;
+    }
+
+    return 0;
+}
