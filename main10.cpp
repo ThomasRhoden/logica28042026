64e669831f638a46e76bd168822bebd841e9beae
ThomasRhoden <rhodenthomas@gmail.com>
2026-04-28 21:46:33 -0300

    10 - function double game

---
+++ main10.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+int doubleGame(int x){
+    return x * 2;
+}
+
+int main(){
+
+    for(int i = 1; i <= 5; i++){
+        std::cout<<"O dobro de "<<i<<" é "<<doubleGame(i)<<std::endl;
+    }
+
+    return 0;
+}
