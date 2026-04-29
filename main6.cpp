6ef74eb453db408da21d9f5eb542b85626e11c17
ThomasRhoden <rhodenthomas@gmail.com>
2026-04-28 21:00:39 -0300

    commit 6

---
+++ main6.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+
+void minhaFuncao(std::string nome){
+    std::cout<<nome<<std::endl;
+}
+
+int main(){
+
+    minhaFuncao("Eduardo");
+    minhaFuncao("Nome");
+
+    return 0;
+}
